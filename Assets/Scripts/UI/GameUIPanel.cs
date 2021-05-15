00334b7822f968667dce59c73ddb92f0f79e1ec1
VincentPC-ZYS <1654209516@qq.com>
2021-05-15 16:21:02 -0700

    1. Updated the basic Main(Menu) UI panel. 2. Updated the basic Game UI panel. 3. Add basic decoupling event system scripts. 4. Add basic button and text scripts for Game UI and Menu UI.

---
+++ Assets/Scripts/UI/GameUIPanel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIPanel : MonoBehaviour
+{
+    private Text currentScore_Txt;
+    private Text diamondCount_Txt;
+    private Button pause_Btn;
+    private Image startGame_Img;
+
+    private void Awake()
+    {
+        EventCenter.AddListner(EventType.ShowGamePanel, ShowGameUIPanel);
+        Initiation();
+    }
+
+    private void Initiation()
+    {
+        currentScore_Txt = transform.Find("CurrentScore_txt").GetComponent<Text>();
+        diamondCount_Txt = transform.Find("DiamondIcon_img/DiamondCount_txt").GetComponent<Text>();
+        pause_Btn = transform.Find("PauseGame_btn").GetComponent<Button>();
+        pause_Btn.onClick.AddListener(OnPauseGameBtnClick);
+        startGame_Img = transform.Find("PauseGame_btn/StartGame_img").GetComponent<Image>();
+        gameObject.SetActive(false);
+        startGame_Img.enabled = false;
+    }
+
+    private void ShowGameUIPanel()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnPauseGameBtnClick()
+    {
+        if(!startGame_Img.enabled)
+        {
+            //TODO PAUSE GAME
+            pause_Btn.gameObject.GetComponent<Image>().enabled = false;
+            startGame_Img.enabled = true;
+        }else
+        {
+            //TODO START GAME
+            pause_Btn.gameObject.GetComponent<Image>().enabled = true;
+            startGame_Img.enabled = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener(EventType.ShowGamePanel, ShowGameUIPanel);
+    }
+}
---
+++ Assets/Scripts/UI/MainUIPanel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIPanel : MonoBehaviour
+{
+    private Button startGame_Btn;
+    private Button skinStore_Btn;
+    private Button rank_Btn;
+    private Button sound_Btn;
+
+    private void Awake()
+    {
+        Initiation();
+    }
+
+    private void Initiation()
+    {
+        startGame_Btn = transform.Find("DownPart_go/StartBg_btn").GetComponent<Button>();
+        startGame_Btn.onClick.AddListener(OnStartGameBtnClick);
+        skinStore_Btn = transform.Find("DownPart_go/OtherBtn_group/SkinStoreBg_btn").GetComponent<Button>();
+        skinStore_Btn.onClick.AddListener(OnSkinStoreBtnClick);
+        rank_Btn = transform.Find("DownPart_go/OtherBtn_group/RankBg_btn").GetComponent<Button>();
+        rank_Btn.onClick.AddListener(OnRankBtnClick);
+        sound_Btn = transform.Find("DownPart_go/OtherBtn_group/SoundBg_btn").GetComponent<Button>();
+        sound_Btn.onClick.AddListener(OnSoundBtnClick);
+    }
+
+    private void OnStartGameBtnClick()
+    {
+        EventCenter.BroadCast(EventType.ShowGamePanel);
+        gameObject.SetActive(false);
+    }
+
+    private void OnSkinStoreBtnClick()
+    {
+
+    }
+
+    private void OnRankBtnClick()
+    {
+
+    }
+
+    private void OnSoundBtnClick()
+    {
+
+    }
+}
